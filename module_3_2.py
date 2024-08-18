d0f2d59faeb73dcd3b9b811ef7d39d2e655ec9ae
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-19 01:03:55 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,16 @@
+def send_email(message, recipient, *, sender):
+    endings = ('.com', '.ru', '.net')
+    if '@' not in recipient or '@' not in sender:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    elif recipient.endswith(endings) is False or sender.endswith(endings) is False:
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+    else:
+        if str(recipient) == str(sender):
+            print('Невозможно отправить письмо самому себе')
+        elif str(sender) != 'university.hel@pgmail.com':
+            print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient)
+        else:
+            print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient)
+
+
+send_email('Сообщение', 'kiss_ka00@mail.ru', sender='university.hel@pgmail.com')

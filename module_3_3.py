2257a51fb8759ce8b5525f0eeef9d0af8e6d225e
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-24 22:48:13 +0300

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -9,3 +9,7 @@ values_list_2 = (True, 7)
 print_params(*values_list)
 print_params(**values_dict)
 print_params(*values_list_2, 42)
+print_params() # Параметры остаются те же, что в функции
+print_params(3, 2) # Меняется значения первых двух аргументов
+print_params(b = 25) # Меняется значение b
+print_params(c = [1,2,3]) # Меняется значение c

6ea3e5a1df70f4eba0df84d732b8985602a66513
Kate <166659076+kate00001@users.noreply.github.com>
2024-08-28 21:08:23 +0300

    Add files via upload

---
+++ module_3_hard.py
@@ -0,0 +1,34 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def data_structure_sum(data_structure):
+    sum_numbers = 0
+    sum_strings = 0
+
+    def addition(data):
+        nonlocal sum_numbers, sum_strings
+        if isinstance(data, list) or isinstance(data, tuple) or isinstance(data, set):
+            for item in data:
+                addition(item)
+        elif isinstance(data, dict):
+            for value in data.values():
+                addition(value)
+            for key in data.keys():
+                addition(key)
+        elif isinstance(data, int) or isinstance(data, str):
+            if isinstance(data, int):
+                sum_numbers += data
+            elif isinstance(data, str):
+                sum_strings += len(data)
+
+    addition(data_structure)
+    return sum_numbers + sum_strings
+
+
+print(data_structure_sum(data_structure))
